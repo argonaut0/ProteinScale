678df6f6fe61e17d18f04092af9193d60b75f460
Jason Hsu <jasonhsu2013@gmail.com>
2021-02-08 20:13:56 -0800

    Clean Up Appearance/Defaults

--- src/App.js
+++ src/App.js
@@ -9,10 +9,16 @@ import Graph from './Graph';
 class App extends Component {
     constructor (props) {
         super(props);
-        this.state = {residueString: "", graphData: [], windowSize: 7};
+        this.state = {
+            residueString: "MLELLPTAVEGVSQAQITGRPEWIWLALGTALMGLGTLYFLVKGMGVSDPDAKKFYAITTLVPAIAFTMYLSMLLGYGLTMVPFGGEQNPIYWARYADWLFTTPLLLLDLALLVDADQGTILALVGADGIMIGTGLVGALTKVYSYRFVWWAISTAAMLYILYVLFFGFTSKAESMRPEVASTFKVLRNVTVVLWSAYPVVWLIGSEGAGIVPLNIETLLFMVLDVSAKVGFGLILLRSRAIFGEAEAPEPSAGDGAAATSD",
+            graphData: [],
+            windowSize: 7,
+            radio: "1"
+        };
         this.updateTextValue = this.updateTextValue.bind(this);
         this.updateData = this.updateData.bind(this);
         this.updateWindowSize = this.updateWindowSize.bind(this);
+        this.updateRadio = this.updateRadio.bind(this);
     }
     
     updateTextValue (event) {
@@ -27,24 +33,28 @@ class App extends Component {
         this.setState( { graphData: KDScale.calculate(this.state.residueString, this.state.windowSize) } );
     }
 
+    updateRadio (event) {
+        this.setState( { radio: event.target.value } );
+    }
+
     render () {
         return (
-            <div>
+            <div className="App">
                 <h1>Protein Scale Graph Generator</h1>
                 <Graph graphData={this.state.graphData} xLabel = "Residue" yLabel = "Hydrophobicity" />
                 <FormControl component="fieldset">
                     <FormLabel component="legend">Scale</FormLabel>
-                    <RadioGroup aria-label="scale" name="scale1" >
+                    <RadioGroup aria-label="scale" name="scale1" onChange={ this.updateRadio } value={this.state.radio}>
                         <FormControlLabel value="1" control={<Radio />} label="K-D Hydrophobicity" />
-                        <FormControlLabel value="2" control={<Radio />} label="Unimplemented" />
-                        <FormControlLabel value="3" control={<Radio />} label="Unimplemented" />
-                        <FormControlLabel value="4" disabled control={<Radio />} label="(Disabled option)" />
+                        <FormControlLabel value="2" disabled control={<Radio />} label="Unimplemented" />
+                        <FormControlLabel value="3" disabled control={<Radio />} label="Unimplemented" />
                     </RadioGroup>
                 </FormControl>
                 <TextField
                     value = { this.state.residueString }
                     label = "Enter 1-letter residue string"
                     multiline
+                    spellCheck={false}
                     onChange = { this.updateTextValue }
                     variant="outlined"
                     type="text"
